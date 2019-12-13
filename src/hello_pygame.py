6cd0e6426bf14bc63f6ce4e2272b60ccac6a4fa9
Jakub Šenkýř <senkyr@spstrutnov.cz>
2019-12-13 13:21:47 +0100

    Zobecněno pro n míčků

--- src/hello_pygame.py
+++ src/hello_pygame.py
@@ -3,20 +3,46 @@
 # 1) pridam do ni knihovnu pygame
 import pygame
 import sys
+import random
 # 2) nastartuju knihovnu
 pygame.init()
 # 3) nastavim okno pro vykreslovani
-rozmer_okna_x = 800
-rozmer_okna_y = 600
+rozmer_okna_x = 1600
+rozmer_okna_y = 900
 okno = pygame.display.set_mode((rozmer_okna_x, rozmer_okna_y))
 
-# 4) uvodni nastaveni moji aplikace
-x = 200
-y = 200
-w = h = 50
+cernobily_rezim = True
+max_velikost_micku = 50
+pocet_micku = 6000
 
-dx = 0.1
-dy = -0.05
+# 4) uvodni nastaveni moji aplikace
+x = []
+y = []
+
+w = []
+h = []
+
+dx = []
+dy = []
+
+rgb = []
+
+for i in range(pocet_micku):
+    # rozmery micku
+    w.append(random.randint(1, max_velikost_micku))
+    h.append(w[i])
+    # pozice micku (omezene velikosti okna)
+    x.append(random.randint(0, rozmer_okna_x - w[i]))
+    y.append(random.randint(0, rozmer_okna_y - h[i]))
+    # rychlosti micku (0 az 1 pixel za frame)
+    dx.append(random.random())
+    dy.append(random.random())
+    # barvy micku (odstiny sede)
+    if cernobily_rezim:
+        odstin = random.randint(0, 254)
+        rgb.append((odstin, odstin, odstin))
+    else:
+        rgb.append((random.randint(0, 254), random.randint(0, 254), random.randint(0, 254)))
 
 # 5) nekonecna vykreslovaci smycka
 while True:
@@ -31,27 +57,32 @@ while True:
     okno.fill((255, 255, 255))
     
     # rovnice pohybu
-    x = x + dx
-    y = y + dy
+    for i in range(len(x)):
+        x[i] = x[i] + dx[i]
+
+    for i in range(len(y)):
+        y[i] = y[i] + dy[i]
     
     # pravidla pro odrazy
-    if y < 0:
-        y = 0
-        dy *= -1
-    if y > rozmer_okna_y - h:
-        y = rozmer_okna_y - h
-        dy *= -1
+    for i in range(len(y)):
+        if y[i] < 0:
+            y[i] = 0
+            dy[i] *= -1
+        if y[i] > rozmer_okna_y - h[i]:
+            y[i] = rozmer_okna_y - h[i]
+            dy[i] *= -1
         
-    if x < 0:
-        x = 0
-        dx *= -1
-    if x > rozmer_okna_x - w:
-        x = rozmer_okna_x - w
-        dx *= -1
+    for i in range(len(x)):
+        if x[i] < 0:
+            x[i] = 0
+            dx[i] *= -1
+        if x[i] > rozmer_okna_x - w[i]:
+            x[i] = rozmer_okna_x - w[i]
+            dx[i] *= -1
     
     # vykresleni tvaru
-    pygame.draw.ellipse(okno, (0, 0, 0), (x, y, w, h))
-    pygame.draw.ellipse(okno, (0, 0, 0), (x, y, w, h))
+    for i in range(len(x)):
+        pygame.draw.ellipse(okno, rgb[i], (x[i], y[i], w[i], h[i]))
     
     # prekresleni okna
     pygame.display.update()
